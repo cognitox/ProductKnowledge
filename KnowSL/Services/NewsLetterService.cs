7be4f6eaa2a7346ee64066fba77adbf1a78df600
davidgrupp <justinjarczyk@gmail.com>
2014-07-23 00:01:28 -0500

    added linq context, and setup newsletter to handle user input

--- KnowSL/Services/NewsLetterService.cs
+++ KnowSL/Services/NewsLetterService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using KnowSL.Enumerations;
+using KnowSL.Database;
+using System.Configuration;
 
 namespace KnowSL.Services
 {
@@ -15,8 +17,20 @@ namespace KnowSL.Services
 
         public PilotSignupStatus CreateOrUpdate(String emailAddress)
         {
-            //throw new NotImplementedException();
-            return PilotSignupStatus.Success;
+            var result = PilotSignupStatus.Failed;
+            try
+            {
+                var context = new PKDatabaseDataContext(ConfigurationManager.ConnectionStrings["DB_9B214B_ProductKnowledgeConnectionString"].ToString());
+
+                context.NewsLetters.InsertOnSubmit(new NewsLetter()
+                {
+                    email = emailAddress
+                });
+                context.SubmitChanges();
+                result = PilotSignupStatus.Success;
+            }
+            catch (Exception) { }
+            return result;
         }
 
     }
